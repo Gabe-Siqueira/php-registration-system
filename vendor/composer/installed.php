c5c14a67d3ea48849619a2fc37a5d638aa189f90
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-09-12 22:49:05 -0300

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ac795d5898b6c616cd6bc6bff0f577f9100c2b55',
+        'name' => 'dev/php-registration-system',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dev/php-registration-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ac795d5898b6c616cd6bc6bff0f577f9100c2b55',
+            'dev_requirement' => false,
+        ),
+    ),
+);
