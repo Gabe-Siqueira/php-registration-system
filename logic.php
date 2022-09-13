154eb33e6a1ca7600714a50dc734843701ca31f5
Gabe-Siqueira <gabriel.snsilva2@gmail.com>
2022-09-12 22:58:48 -0300

    Screen logic

---
+++ logic.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+define('TITLE','Exercício de Lógica');
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/formLogic.php';
+include __DIR__.'/includes/footer.php';
